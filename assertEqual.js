fafe95a9647621554d8c283ad52ee6ad09f94719
aliyasser20 <aliyasser20@hotmail.com>
2020-03-02 13:58:10 -0500

    head

--- assertEqual.js
+++ assertEqual.js
@@ -2,8 +2,3 @@ const assertEqual = (actual, expected) => {
   const message = actual === expected ? `✅✅✅ ${actual} === ${expected}` : `🛑🛑🛑 ${actual} !== ${expected}`;
   console.log(message);
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
