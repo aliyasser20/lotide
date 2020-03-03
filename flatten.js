802cfff3cb5b485ef8102d4ceac6c8dbdb53348e
aliyasser20 <aliyasser20@hotmail.com>
2020-03-03 14:31:49 -0500

    finished flatten

--- flatten.js
+++ flatten.js
@@ -14,3 +14,18 @@ const assertArraysEqual = (array1, array2) => {
   const message = eqArrays(array1, array2) ? `✅✅✅ ${array1} === ${array2}` : `🛑🛑🛑 ${array1} !== ${array2}`;
   console.log(message);
 };
+
+const flatten = (inputArray) => {
+  const outputArea = [];
+  inputArray.forEach(el => {
+    if (Array.isArray(el)) {
+      outputArea.push(...el);
+    } else {
+      outputArea.push(el);
+    }
+  });
+  return outputArea;
+};
+
+assertArraysEqual(flatten([1, 2, 4, [6, 1, 4], 7]), [1, 2, 4, 6, 1, 4, 7]);
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
