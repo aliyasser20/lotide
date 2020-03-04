69492ebf2d7f8ef537273f4735ee123deaa24c53
aliyasser20 <aliyasser20@hotmail.com>
2020-03-04 09:42:58 -0500

    completed countOnly

---
+++ countOnly.js
@@ -0,0 +1,40 @@
+const assertEqual = (actual, expected) => {
+  const message = actual === expected ? `✅✅✅ ${actual} === ${expected}` : `🛑🛑🛑 ${actual} !== ${expected}`;
+  console.log(message);
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  
+  const outputObject = {};
+
+  for (let key in itemsToCount) {
+    if (itemsToCount.hasOwnProperty(key)) {
+      if (itemsToCount[key] && allItems.includes(key)) {
+        outputObject[key] = 0;
+        allItems.forEach(item => {
+          if (item === key) outputObject[key] += 1;
+        });
+      }
+    }
+  }
+
+  return outputObject;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
