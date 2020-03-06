9eb6dffd1f5e1a67812cbc62f3aa53b1790f12da
aliyasser20 <aliyasser20@hotmail.com>
2020-03-06 11:50:26 -0500

    added recursion

--- eqArrays.js
+++ eqArrays.js
@@ -7,7 +7,9 @@ const eqArrays = (array1, array2) => {
   if (array1.length !== array2.length) return false;
   let result;
   array1.forEach((el,index) => {
-    if (result !== false) {
+    if (Array.isArray(el)) {
+      result = eqArrays(el, array2[index]);
+    } else if (result !== false) {
       result = el === array2[index];
     }
   });
@@ -15,7 +17,13 @@ const eqArrays = (array1, array2) => {
   return result;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+assertEqual(eqArrays([[2, 3], [4 , [10, [12, 16]]]], [[2, 3], [4 , [10, [12, 16]]]]), true); // => false
