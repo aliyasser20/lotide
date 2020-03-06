590b36accd73721ba387c1271114b469c7147fb8
aliyasser20 <aliyasser20@hotmail.com>
2020-03-06 11:41:55 -0500

    completed eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -40,7 +40,10 @@ const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 
 
-console.log(eqObjects(ab, ba)); // => true
-console.log(eqObjects(ab, abc)); // => false
-console.log(eqObjects(cd, dc)); // => true
-console.log(eqObjects(cd, cd2)); // => false
+// console.log(eqObjects(ab, ba)); // => true
+// console.log(eqObjects(ab, abc)); // => false
+// console.log(eqObjects(cd, dc)); // => true
+// console.log(eqObjects(cd, cd2)); // => false
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }));// => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }));// => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
